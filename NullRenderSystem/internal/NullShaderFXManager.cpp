886ed7d49ad644eebf0e29ce99c50151af39b36f
Myway <MSoft1115@hotmail.com>
2015-11-15 14:53:36 +0800

    ...

--- NullRenderSystem/internal/NullShaderFXManager.cpp
+++ NullRenderSystem/internal/NullShaderFXManager.cpp
@@ -5,17 +5,15 @@ namespace Rad {
 	//
 	NullShaderFXManager::NullShaderFXManager()
 	{
-		mNullShaderFX = new ShaderFX("Null", "Null", "");
-		mNullShaderFX->AddPass(new NullShaderPass);
 	}
 
 	NullShaderFXManager::~NullShaderFXManager()
 	{
-		delete mNullShaderFX;
 	}
 
-	ShaderFX * NullShaderFXManager::Load(const String & name, const String & source, const String & macros)
+	void NullShaderFXManager::_loadImp(ShaderFX * fx, DataStreamPtr stream)
 	{
-		return mNullShaderFX;
+		fx->AddPass(new NullShaderPass);
 	}
+
 }
--- NullRenderSystem/internal/NullShaderFXManager.h
+++ NullRenderSystem/internal/NullShaderFXManager.h
@@ -16,18 +16,8 @@ namespace Rad {
 		NullShaderFXManager();
 		virtual ~NullShaderFXManager();
 
-		virtual ShaderFX * 
-			Load(const String & name, const String & source, const String & macros);
-		virtual void 
-			Remove(ShaderFX * fx) {}
 		virtual void
-			Reload(ShaderFX * fx) {}
-		virtual void 
-			ReloadAll() {}
-
-
-	protected:
-		ShaderFX * mNullShaderFX;
+			_loadImp(ShaderFX * fx, DataStreamPtr stream);
 	};
 
 }
